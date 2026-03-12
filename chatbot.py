35f7428462cb7831cf9378fb83892c9c56543ba5
TemTemPrime <ephraimplayz@gmail.com>
2026-03-12 11:23:24 +0100

    that's a strange reason to not work bro

--- chatbot.py
+++ chatbot.py
@@ -1,15 +1,13 @@
 import os
 import streamlit as st
-from google import genai
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_core.documents import Document
-from dotenv import load_dotenv
+
 import json
-import faiss
-from langchain_community.docstore.in_memory import InMemoryDocstore
+
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 api_key = st.secrets.get("GEMINI_API_KEY")
